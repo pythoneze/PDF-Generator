97b1155ef189863a038bc468892a132d25fdb779
pythoneze <pythoneze@gmail.com>
2024-08-28 17:31:45 -0400

    Add lines

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ df = pd.read_csv("topics.csv")
 def create_header(topic):
     pdf.set_font(family="Times", style="B", size=16)
     pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row['Topic'], ln=True)
-    pdf.line(10, 20, 200, 20)
+    pdf.cell(w=0, h=12, txt=row['Topic'], ln=1)
 
 def create_footer(topic):
     pdf.set_y(-10) 
@@ -18,14 +17,22 @@ def create_footer(topic):
     pdf.set_text_color(100, 180, 100)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
+def create_lines(y_position):
+    pdf.line(10, y_position, 200, y_position)
+
 for _, row in df.iterrows():
     # Header
     pdf.add_page()
     create_header(row["Topic"])
+    for y in range(20, 290, 10):        
+        create_lines(y)
+
     create_footer(row["Topic"])
 
     for _ in range(row["Pages"] - 1):
         pdf.add_page()
+        for y in range(20, 290, 10):        
+            create_lines(y)
         create_footer(row["Topic"])
 
 pdf.output("output.pdf")
